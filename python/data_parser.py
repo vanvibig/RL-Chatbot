26e416389788a9849cffd73653a4c051b8643932
Kudo Vĩ <vanvibig@gmail.com>
2019-02-25 12:08:47 +0700

    Update data_parser.py

--- python/data_parser.py
+++ python/data_parser.py
@@ -76,8 +76,11 @@ def parse_all_words(all_words_path):
 
 """ Extract only the vocabulary part of the data """
 def refine(data):
-    words = re.findall("[a-zA-Z'-]+", data)
-    words = ["".join(word.split("'")) for word in words]
+#    words = re.findall("[a-zA-Z'-]+", data)
+    # data = "Anh yêu em nhiều lắm"
+    rgx = re.compile("([\w][\w']*\w)")
+    words = rgx.findall(data)
+    words = ["".join(word.split(" ")) for word in words]
     # words = ["".join(word.split("-")) for word in words]
     data = ' '.join(words)
     return data
